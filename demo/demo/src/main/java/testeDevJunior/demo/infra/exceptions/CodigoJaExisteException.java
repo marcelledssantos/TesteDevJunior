abfe11de6960ea9b3ed065e3653080996dea7a1e
marcelledssantos <marcelledossantos2@gmail.com>
2025-02-05 01:05:39 -0300

    Limpeza no codigo

--- demo/demo/src/main/java/testeDevJunior/demo/infra/exceptions/CodigoJaExisteException.java
+++ demo/demo/src/main/java/testeDevJunior/demo/infra/exceptions/CodigoJaExisteException.java
@@ -1,5 +1,9 @@
 package testeDevJunior.demo.infra.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class CodigoJaExisteException extends RuntimeException {
         public CodigoJaExisteException(String mensagem) {
             super(mensagem);
